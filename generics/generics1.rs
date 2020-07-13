52cd72b7142a5a9619810d8ce24a27aa512ca569
lirui <ruilibuaa@sina.com>
2020-07-13 23:05:23 +0800

    Add files via upload

---
+++ generics/generics1.rs
@@ -0,0 +1,9 @@
+// This shopping list program isn't compiling! 
+// Use your knowledge of generics to fix it.
+
+
+fn main() {
+    let mut shopping_list: Vec<String> = Vec::new();
+    shopping_list.push(String::from("milk"));
+}
+
